b76a1b3b7e1ae21dc2bf33056045ef3a57c26e1b
王冲 <614713281@qq.com>
2020-05-16 18:12:36 +0800

    '自我校验'

--- Fresh/users/urls.py
+++ Fresh/users/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from django.conf.urls import url
 
 # urlpatterns是被Django自动识别的路由列表变量：定义该应用的所有路由信息
 urlpatterns = [
@@ -9,5 +10,11 @@ urlpatterns = [
     # path('users/register/', views.register),
     # 注意：给类视图配置路由，需要将类试图转换成为函数视图，用as_view方法
     # path('网络地址正则表达式', 类视图名.as_view()),
-    path('users/register/', views.RegisterView.as_view())
+    path('users/register/', views.RegisterView.as_view()),
+    # 对re_path()和url()没有封装正则表达式的固定开始和结束符号
+    # r使字符串中的/不发生转义
+    re_path(r'^users/login/$', views.LoginView.as_view()),
+    # 注意：url和re_path匹配地址的正则没有严格的写完整，可能造成路由屏蔽
+    url(r"^users/say/$", views.SayView.as_view()),
+    path('users/sayhello/', views.SayHelloView.as_view()),
 ]
--- Fresh/users/views.py
+++ Fresh/users/views.py
@@ -6,12 +6,12 @@ from django.views import View
 
 
 # 创建函数视图
-# def register(request):
-#     """用户注册函数视图"""
-#     # :param request:请求对象，包含了请求报文信息
-#     # return：响应对象，用于构造响应报文，并响应给用户
-#     # 响应数据
-#     return http.HttpResponse("这是第一个注册页面函数试图诞生地")
+def register(request):
+    """用户注册函数视图"""
+    # :param request:请求对象，包含了请求报文信息
+    # return：响应对象，用于构造响应报文，并响应给用户
+    # 响应数据
+    return http.HttpResponse("这是第一个注册页面函数试图诞生地")
 
 
 # 定义类视图
@@ -20,13 +20,47 @@ from django.views import View
 #       return 响应对象
 
 
-class RegisterView(View):
+class ListModelMixin():
+    """自定义的扩展功能，扩展一个输出的功能
+    注意：python中定义扩展类，一般以Mixin结尾"""
+    def list(self, request):
+        """扩展功能实现"""
+        tmp = ['1', '2', '3']
+        print(tmp)
+
+
+# 继承扩展类，增加功能
+class RegisterView(View, ListModelMixin):
     """用户注册类视图
     get,post: http://127.0.0.1:8000/users/register
     只能接收跟定义的方法同名的请求"""
 
     def get(self, request):
+        # 调用扩展类的方法
+        self.list(request)
         return http.HttpResponse("这里假装是有一个注册页面")
 
     def post(self, request):
-        return http.HttpResponse("这是这侧逻辑实现函数")
+        self.list(request)
+        return http.HttpResponse("这是逻辑实现函数")
+
+
+class SayView(View):
+    """测试路由屏蔽"""
+    def get(self, request):
+        return http.HttpResponse('你好')
+
+
+class SayHelloView(View):
+    """测试路由屏蔽"""
+    def get(self, request):
+        return http.HttpResponse('你也好')
+
+
+class LoginView(View):
+    """用户登录视图"""
+    def get(self, request):
+        return http.HttpResponse('这是一个用户登录视图')
+
+
+
